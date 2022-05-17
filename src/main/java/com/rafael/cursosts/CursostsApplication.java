624d16f4e2755957665a7381d0addcc7ad683d0a
Rafael Trindade R. Machado <rafael.tr.machado@gmail.com>
2022-05-16 22:45:46 -0300

    Projeto criado

---
+++ src/main/java/com/rafael/cursosts/CursostsApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.cursosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/cursosts/CursostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.cursosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
